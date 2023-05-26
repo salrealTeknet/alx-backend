80ae49f38df337bb95c1ab3901d8a1cedb5cc545
salrealTeknet <salihuabduljawwad.a@gmail.com>
2023-05-26 20:24:30 +0000

    add corrections

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,41 +1,47 @@
 #!/usr/bin/env python3
-""" Basic Flask app, Basic Babel setup, Get locale from request,
-    Parametrize templates, Force locale with URL parameter """
-from flask import Flask, render_template, request
-from flask_babel import Babel, gettext
+""" Route module for the API """
+from flask import Flask, request, render_template
+from flask_babel import Babel
+from os import getenv
 
 app = Flask(__name__)
 babel = Babel(app)
-""" instantiate the Babel object """
 
 
 class Config(object):
-    """ config class """
+    """ Babel configuration """
     LANGUAGES = ['en', 'fr']
+    # these are the inherent defaults just btw
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
-app.config.from_object(Config)
-""" Use that class as config for Flask app """
+# set the above class object as the configuration for the app
+app.config.from_object('4-app.Config')
 
 
-@app.route('/')
-def root():
-    """ basic Flask app """
-    return render_template("4-index.html")
+@app.route('/', methods=['GET'], strict_slashes=False)
+def index() -> str:
+    """ GET /
+    Return:
+      - 4-index.html
+    """
+    return render_template('4-index.html')
 
 
 @babel.localeselector
-def get_locale():
-    """ to determine the best match with our supported languages """
-    localLang = request.args.get('locale')
-    supportLang = app.config['LANGUAGES']
-    if localLang in supportLang:
-        return localLang
+def get_locale() -> str:
+    """ Determines best match for supported languages """
+    # check if there is a locale parameter/query string
+    if request.args.get('locale'):
+        locale = request.args.get('locale')
+        if locale in app.config['LANGUAGES']:
+            return locale
     else:
         return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 if __name__ == "__main__":
-    app.run()
+    host = getenv("API_HOST", "0.0.0.0")
+    port = getenv("API_PORT", "5000")
+    app.run(host=host, port=port)
